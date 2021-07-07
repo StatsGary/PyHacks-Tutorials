89343b969b828f370d7207e38ee6edde71fa9039
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2021-07-07 20:01:36 +0100

    Add files via upload

---
+++ 05_ConditionalLogic.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# Title             PyHacks - Conditional Logic - If Else Elseif 
+# Author            Gary Hutson aka hutsons-hacks.info
+# Date created      07/07/2021
+# =============================================================================
+# =============================================================================
+# Logical Operators
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+# =============================================================================
+
+# ------------- Simple If Statement  ------------------#
