09ca48ab46e0705393de8a6e6acf6cdf28020825
Gary Hutson <g.hutson@nhs.net>
2021-07-14 17:10:34 +0100

    Added Pandas

--- 21_Pandas/21_3_DataFrames.py
+++ 21_Pandas/21_3_DataFrames.py
@@ -3,17 +3,13 @@
 # Author            Gary Hutson aka hutsons-hacks.info
 # Date created      14/07/2021
 # =============================================================================
-
 import pandas as pd
-
 ip_data = {
         "ip_admits": [213,343,124],
         "ed_attends": [456,343,324]
     }
-
 ip_df = pd.DataFrame(ip_data)
 print(ip_df)
-
 #------------------- LOCATE ROWS BY LOCATION AND INDEX LOCATION----------------
 
 # Locate rows
@@ -26,8 +22,17 @@ print(ip_df.iloc[-1]) #Use iloc to get an integer index
 print(ip_df.iloc[-len(ip_df)])
 
 #------------------- LOCATE BY INDEXING ---------------------------------------
+df = pd.DataFrame(ip_data, index = ["Mon", "Tues", "Wed"])
+print(df) 
 
-idx_df = pd.DataFrame(ip_df, index=["Mon", "Tues", "Wed"])
-print(idx_df)
+# Locate with loc
+print(df.loc["Mon":"Wed"]) #Slice between Monday and wednesday
+print(df.loc[["Mon", "Wed"]]) #Get Monday and Wednesday, not Tuesday
 
+#------------------- READ IN CSV ---------------------------------------
+stranded_data = pd.read_csv("Data/Stranded/stranded.csv")
+print(stranded_data.head())
+print(stranded_data.tail(20))
+print(stranded_data.columns)
+print(stranded_data.info())
 
