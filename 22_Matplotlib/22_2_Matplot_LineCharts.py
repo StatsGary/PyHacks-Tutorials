92adf3e726c74ffb66d8549889776a2d9402e9ca
Gary Hutson <g.hutson@nhs.net>
2021-07-15 15:32:48 +0100

    Update 22_2_Matplot_LineCharts.py

--- 22_Matplotlib/22_2_Matplot_LineCharts.py
+++ 22_Matplotlib/22_2_Matplot_LineCharts.py
@@ -4,5 +4,12 @@
 # Date created      15/07/2021
 # =============================================================================
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.array([10,40, 34, 54, 65, 32, 89, 25, 45, 56])
+
+# Line styles
+
 
 
