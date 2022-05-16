39f01a9c1d3235b8db7326a087d5d1c560d63ab6
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2022-05-16 12:19:47 +0100

    Added two tutorials

---
+++ 25_Youtube_downloader.py
@@ -0,0 +1,7 @@
+# Install and download
+# PyTube - clone
+#python -m pip install .
+
+from pytube import YouTube
+# link of the video to be downloaded 
+link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
