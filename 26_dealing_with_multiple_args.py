bd1ea6c0a3913ad78a7514a3ede0fc5a2cab8a9d
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2022-05-16 12:49:46 +0100

    Kwargs and args

---
+++ 26_dealing_with_multiple_args.py
@@ -0,0 +1,35 @@
+# =============================================================================
+# Title             PyHacks - Dealing with multiple arguments
+# Author            Gary Hutson aka hutsons-hacks.info
+# Date created      16/05/2022
+# =============================================================================
+
+def test_mult_arguments(norm_arg, *args_values):
+    print(f'First argument is {norm_arg}')
+    # Iterated through additional arguments and print
+    for arg in args_values:
+        print('Passed another argument (*arg_values):', arg)
+
+# Let's test our function to capture original passed argument and additional arguments
+
+test_mult_arguments('this is my normal argument', 'additional argument 1', 'additional argument 2',
+                    'additional argument 3')
+
+
+# Work with kwargs
+
+def working_with_kwargs(**my_k_wargs):
+    for key, value in my_k_wargs.items():
+        print('Arguments passed:\n', '-'*80)
+        print('{0}={1}'.format(key, value))
+
+        if key == 'fav_food':
+            print(f'Item contains his favourite food {value}.')
+
+        if key == 'first_name':
+            print(f'His name is {value}')
+
+
+
+working_with_kwargs(first_name='Gary', occupation='ML Engineer Lead', 
+                    fav_food='Fish')
